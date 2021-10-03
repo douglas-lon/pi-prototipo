710563c305d744f3314f3538573fcf0934328b8f
douglas-lon <playerdoug60@gmail.com>
2021-10-03 12:00:08 -0300

    cria tabela aluno e notaaluno

--- pi/models.py
+++ pi/models.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import date
+from enum import unique
 from pi import db, login_manager
 from flask_login import UserMixin
 
@@ -29,3 +30,26 @@ class Materia(db.Model):
 
     def __repr__(self):
         return f"Materia('{self.id}', '{self.nome}')"
+
+
+
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ra = db.Column(db.Integer, unique=True, nullable=False)
+    nome = db.Column(db.String(20), nullable=False)
+    sobrenome = db.Column(db.String(100), nullable=False)
+    turma = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"Aluno('{self.id}', '{self.ra}', '{self.nome}')"
+
+class NotaAluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_aluno = db.Column(db.Integer, db.ForeignKey('aluno.id'), nullable=False)
+    id_materia = db.Column(db.Integer, db.ForeignKey('materia.id'), nullable=False)
+    nota = db.Column(db.String(5), nullable=False)
+    bimestre = db.Column(db.Integer, nullable=False)
+    ano = db.Column(db.SmallInteger, nullable=False, default=date.today().year)
+
+    def __repr__(self):
+        return f"Nota('{self.id}', '{self.id_aluno}', '{self.id_materia}', '{self.nota}', '{self.bimestre}')"
